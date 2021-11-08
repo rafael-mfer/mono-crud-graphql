b152945803eeb862136dadb2dc4866a05bfa2084
Rafael Maximiano Ferreira <rafael_mfer@hotmail.com>
2021-11-08 19:58:26 -0300

    Importando as dependências para o app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 import { MoviesModule } from './movies/movies.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+  GraphQLModule.forRoot({
+    autoSchemaFile: 'schema.gql',
+  }),
+  MoviesModule,
+  MongooseModule.forRoot('mongodb://localhost/nestgraphql')],
   controllers: [AppController],
   providers: [AppService],
 })
