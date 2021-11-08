515dd3de0f9a54226be9dd921aee1cf36237be23
Rafael Maximiano Ferreira <rafael_mfer@hotmail.com>
2021-11-08 20:44:44 -0300

    Queries e mutations no resolver do GraphQL

--- src/movies/movies.resolver.ts
+++ src/movies/movies.resolver.ts
@@ -1,4 +1,33 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { MoviesService } from './movies.service';
+import { MovieType } from './dto/create-movie.dto';
+import { MovieInput } from './input-movies.input';
+import { Movie } from './interfaces/movie.interface'
 
-@Resolver()
-export class MoviesResolver {}
+@Resolver(of => MovieType)
+export class MoviesResolver {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Query(returns  => [MovieType])
+  async movies(): Promise<MovieType[]> {
+    return this.moviesService.findAll();
+  }
+
+  @Mutation(returns => MovieType)
+  async createMovie(@Args('input') input: MovieInput): Promise<MovieType> {
+    return this.moviesService.create(input);
+  }
+
+  @Mutation(returns => MovieType)
+  async updateMovie(
+    @Args('id') id: string,
+    @Args('input') input: MovieInput,
+  ) {
+    return this.moviesService.update(id, input as unknown as Movie);
+  }
+
+  @Mutation(returns  => MovieType)
+  async deleteMovie(@Args('id') id: string) {
+    return this.moviesService.delete(id);
+  }
+}
