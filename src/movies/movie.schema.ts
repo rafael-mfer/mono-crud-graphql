c8813f06dccb2a071fd07b45bd3e2ab9b9bb3e92
Rafael Maximiano Ferreira <rafael_mfer@hotmail.com>
2021-11-10 10:53:28 -0300

    alteração releaseDate p/ year(int) c/ 4 caracteres

--- src/movies/movie.schema.ts
+++ src/movies/movie.schema.ts
@@ -1,7 +1,17 @@
 import * as mongoose from 'mongoose';
 
 export const MovieSchema = new mongoose.Schema({
- title: String,
- director: String,
- releaseDate: Date,
+  title: String,
+  director: String,
+  year: {
+    type: Number,
+    validate: {
+      validator: function (val: {
+        toString: () => { (): any; new (): any; length: number };
+      }) {
+        return val.toString().length === 4;
+      },
+      message: (val: { value: any }) => `${val.value} has to be 4 digits`,
+    },
+  },
 });
